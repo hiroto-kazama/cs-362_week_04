295fd170c53a3d2667526c698d33c89d09b9d5e6
hiroto <kazamah@oregonstate.edu>
2021-04-25 16:24:28 -0700

    with erro handling

--- CS-362_assignment_01.py
+++ CS-362_assignment_01.py
@@ -1,7 +1,15 @@
 def divcheck(m,n):
     return True if m % n == 0 else False
 
+
 x = int(input("Input: "))
+
+"""while x < 0:
+    print("must be a positive integer")
+    x = int(input("Input: "))
+    if x > 0:
+        break"""
+
 
 if (divcheck(x,4)):
     if (divcheck(x,100)):
