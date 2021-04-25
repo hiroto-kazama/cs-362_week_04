88c69c57e42d7734fb8d574286a81623bf5f8495
hiroto <73141170+hiroto-kazama@users.noreply.github.com>
2021-04-25 15:55:19 -0700

    Add files via upload

---
+++ CS-362_assignment_01.py
@@ -0,0 +1,16 @@
+def divcheck(m,n):
+    return True if m % n == 0 else False 
+
+x = int(input("Input: "))
+
+if (divcheck(x,4)):
+    if (divcheck(x,100)):
+        if (divcheck(x,400)):
+            print("Output - ", x, "is a leap year")
+        else:
+            print("Output - ", x, "is not a leap year")
+    else:
+        print("Output - ", x, "is  a leap year")
+else:
+    print("Output - ", x, "is not a leap year")
+
